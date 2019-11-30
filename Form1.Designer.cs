b363c4cd906267f562440d1950fc4c931b135324
kYasinAblay <58375357+kYasinAblay@users.noreply.github.com>
2019-12-01 02:52:07 +0300

    Tarihleri birbirinden çıkarma kodları

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace sorguOrnegi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGun = new System.Windows.Forms.Label();
+            this.lblAy = new System.Windows.Forms.Label();
+            this.lblYil = new System.Windows.Forms.Label();
+            this.txtGun2 = new System.Windows.Forms.TextBox();
+            this.txtAy2 = new System.Windows.Forms.TextBox();
+            this.txtYil2 = new System.Windows.Forms.TextBox();
+            this.btnTarihBugun = new System.Windows.Forms.Button();
+            this.txtGun = new System.Windows.Forms.TextBox();
+            this.txtAy = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGun
+            // 
+            this.lblGun.AutoSize = true;
+            this.lblGun.Location = new System.Drawing.Point(281, 311);
+            this.lblGun.Name = "lblGun";
+            this.lblGun.Size = new System.Drawing.Size(35, 17);
+            this.lblGun.TabIndex = 21;
+            this.lblGun.Text = "Gün";
+            // 
+            // lblAy
+            // 
+            this.lblAy.AutoSize = true;
+            this.lblAy.Location = new System.Drawing.Point(213, 311);
+            this.lblAy.Name = "lblAy";
+            this.lblAy.Size = new System.Drawing.Size(24, 17);
+            this.lblAy.TabIndex = 20;
+            this.lblAy.Text = "Ay";
+            // 
+            // lblYil
+            // 
+            this.lblYil.AutoSize = true;
+            this.lblYil.Location = new System.Drawing.Point(145, 311);
+            this.lblYil.Name = "lblYil";
+            this.lblYil.Size = new System.Drawing.Size(23, 17);
+            this.lblYil.TabIndex = 19;
+            this.lblYil.Text = "Yıl";
+            // 
+            // txtGun2
+            // 
+            this.txtGun2.Location = new System.Drawing.Point(276, 101);
+            this.txtGun2.Name = "txtGun2";
+            this.txtGun2.Size = new System.Drawing.Size(74, 22);
+            this.txtGun2.TabIndex = 18;
+            // 
+            // txtAy2
+            // 
+            this.txtAy2.Location = new System.Drawing.Point(208, 101);
+            this.txtAy2.Name = "txtAy2";
+            this.txtAy2.Size = new System.Drawing.Size(74, 22);
+            this.txtAy2.TabIndex = 17;
+            // 
+            // txtYil2
+            // 
+            this.txtYil2.Location = new System.Drawing.Point(137, 101);
+            this.txtYil2.Name = "txtYil2";
+            this.txtYil2.Size = new System.Drawing.Size(74, 22);
+            this.txtYil2.TabIndex = 16;
+            // 
+            // btnTarihBugun
+            // 
+            this.btnTarihBugun.Location = new System.Drawing.Point(357, 61);
+            this.btnTarihBugun.Name = "btnTarihBugun";
+            this.btnTarihBugun.Size = new System.Drawing.Size(67, 22);
+            this.btnTarihBugun.TabIndex = 15;
+            this.btnTarihBugun.Text = "..";
+            this.btnTarihBugun.UseVisualStyleBackColor = true;
+            this.btnTarihBugun.Click += new System.EventHandler(this.btnTarihBugun_Click);
+            // 
+            // txtGun
+            // 
+            this.txtGun.Location = new System.Drawing.Point(276, 61);
+            this.txtGun.Name = "txtGun";
+            this.txtGun.Size = new System.Drawing.Size(74, 22);
+            this.txtGun.TabIndex = 14;
+            // 
+            // txtAy
+            // 
+            this.txtAy.Location = new System.Drawing.Point(208, 61);
+            this.txtAy.Name = "txtAy";
+            this.txtAy.Size = new System.Drawing.Size(74, 22);
+            this.txtAy.TabIndex = 13;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Location = new System.Drawing.Point(137, 61);
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(74, 22);
+            this.txtYil.TabIndex = 12;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(137, 151);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(103, 36);
+            this.btnHesapla.TabIndex = 11;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Gün";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Ay";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 17);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Yıl";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 303);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Sonuç :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 390);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblGun);
+            this.Controls.Add(this.lblAy);
+            this.Controls.Add(this.lblYil);
+            this.Controls.Add(this.txtGun2);
+            this.Controls.Add(this.txtAy2);
+            this.Controls.Add(this.txtYil2);
+            this.Controls.Add(this.btnTarihBugun);
+            this.Controls.Add(this.txtGun);
+            this.Controls.Add(this.txtAy);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblGun;
+        internal System.Windows.Forms.Label lblAy;
+        internal System.Windows.Forms.Label lblYil;
+        internal System.Windows.Forms.TextBox txtGun2;
+        internal System.Windows.Forms.TextBox txtAy2;
+        internal System.Windows.Forms.TextBox txtYil2;
+        internal System.Windows.Forms.Button btnTarihBugun;
+        internal System.Windows.Forms.TextBox txtGun;
+        internal System.Windows.Forms.TextBox txtAy;
+        internal System.Windows.Forms.TextBox txtYil;
+        internal System.Windows.Forms.Button btnHesapla;
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sorguOrnegi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime zaman = DateTime.Now;
+        int yil1 = 0, ay1 = 0, gun1 = 0, yil2 = 0, ay2 = 0, gun2 = 0, sonucYil = 0, sonucAy = 0, sonucGun = 0;
+
+
+        private void btnTarihBugun_Click(object sender, EventArgs e)
+        {
+
+            yil1 = zaman.Year;
+            ay1 = zaman.Month;
+            gun1 = zaman.Day;
+
+            txtYil.Text = yil1.ToString();
+            txtAy.Text = ay1.ToString();
+            txtGun.Text = gun1.ToString();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            yil2 = Convert.ToInt16(txtYil2.Text);
+            ay2 = Convert.ToInt16(txtAy2.Text);
+            gun2 = Convert.ToInt16(txtGun2.Text);
+
+
+            if (txtYil.Text == "" || txtAy.Text == "" || txtGun.Text == "" || txtYil2.Text == "" || txtAy2.Text == "" || txtGun2.Text == "")
+            {
+                MessageBox.Show("Alanlar boş geçilemez!");
+                return;
+            }
+            if (yil1 >= yil2)
+            {
+                if (gun1 >= gun2)
+                {
+
+                    sonucGun = gun1 - gun2;
+
+                }
+                else
+                {
+                    if (ay1 == ay2)
+                    {
+                        sonucGun = gun2 - gun1;
+                    }
+                    else
+                    {
+                        ay1--;
+                        gun1 += 30;
+                        sonucGun = gun1 - gun2;
+                    }
+
+                }
+
+
+                if (ay1 >= ay2)
+                {
+                    sonucAy = ay1 - ay2;
+                }
+                else
+                {
+                    yil1--;
+                    ay1 += 12;
+                    sonucAy = ay1 - ay2;
+
+                }
+
+                sonucYil = yil1 - yil2;
+            }
+            else
+            {
+                if (gun2 >= gun1)
+                {
+                    sonucGun = gun2 - gun1;
+                }
+                else
+                {
+                    ay2--;
+                    gun2 += 30;
+                    sonucGun = gun2 - gun1;
+                }
+                if (ay2 >= ay1)
+                {
+                    sonucAy = ay2 - ay1;
+                }
+                else
+                {
+                    yil2--;
+                    ay2 += 12;
+                    sonucAy = ay2 - ay1;
+
+                }
+
+                sonucYil = yil2 - yil1;
+
+            }
+
+            lblYil.Text = sonucYil.ToString();
+            lblAy.Text = sonucAy.ToString();
+            lblGun.Text = sonucGun.ToString();
+
+        }
+    }
+}
